af46a50b0274af62f3e2e497e9b2312a89db2844
Istiaque Chowdhury <istiaquechowdhury430@gmail.com>
2024-06-09 17:39:29 +0600

    removed existing folder and added a new folder

---
+++ Istiaque.Web/Istiaque/Controllers/PortfolioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Istiaque.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+    }
+}
---
+++ Istiaque.Web/Istiaque/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Istiaque.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
